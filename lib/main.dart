3ac5d0ced0096435b874c42af7f871c1340a29ff
Stenio Oliveira <steniooliv@gmail.com>
2021-03-31 00:42:35 -0300

    create scafoold

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
     return MaterialApp(
-      home: Text("Hello Flutter"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Quiz")),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            ElevatedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
