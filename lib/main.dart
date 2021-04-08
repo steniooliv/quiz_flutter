3ad5c319a7a1b28948b4351f046106fe3f03de09
Stenio Oliveira <steniooliv@gmail.com>
2021-04-07 23:45:24 -0300

    add quiz and result component

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:quiz_flutter/answer.dart';
-import 'package:quiz_flutter/questions.dart';
+import 'package:quiz_flutter/quiz.dart';
+import 'package:quiz_flutter/result.dart';
 
 main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
 
-  void _responder() {
+  final _perguntas = const [
+    {
+      "texto": "Qual nome da esposa de Luke Skywalker?",
+      "respostas": [
+        {"texto": "Bianezzis Mathias", "pontuacao": 0},
+        {"texto": "Mara Jade", "pontuacao": 10},
+        {"texto": "Mia Coutera", "pontuacao": 0},
+        {"texto": "Jadire Had", "pontuacao": 0},
+      ],
+    },
+    {
+      "texto": "Onde nasceu Sheldon em TBT?",
+      "respostas": [
+        {"texto": "Pará", "pontuacao": 0},
+        {"texto": "Mississipi", "pontuacao": 0},
+        {"texto": "Nova Iorque", "pontuacao": 0},
+        {"texto": "Texas", "pontuacao": 10},
+      ],
+    },
+    {
+      "texto": "Qual nome do mostro em Stranger Things?",
+      "respostas": [
+        {"texto": "Zordon", "pontuacao": 0},
+        {"texto": "Pedagorgon", "pontuacao": 0},
+        {"texto": "Demargongon", "pontuacao": 10},
+        {"texto": "Psicorgon", "pontuacao": 0},
+      ],
+    },
+  ];
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+  }
+
+  void _reiniciarQuestionario() {
     setState(() {
-      _perguntaSelecionada++;
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
     });
-    print("Pergunta Respondida");
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        "texto": "Qual é sua cor favorita?",
-        "respostas": ["Preto", "Vermelho", "Verde", "Azul"],
-      },
-      {
-        "texto": "Qual é sua animal favorito?",
-        "respostas": ["Coelho", "Cobra", "Gato", "Cachorro"],
-      },
-      {
-        "texto": "Qual é seu instrutor favorito?",
-        "respostas": ["Maria", "João", "Stenio", "Pedro"],
-      },
-    ];
-
-    List<String> resposta = perguntas[_perguntaSelecionada]["respostas"];
-    List<Widget> respostas =
-        resposta.map((t) => Resposta(t, _responder)).toList();
-
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
@@ -48,14 +73,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
             ),
           ),
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(
-              perguntas[_perguntaSelecionada]["texto"],
-            ),
-            ...respostas,
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                responder: _responder,
+                perguntaSelecionada: _perguntaSelecionada,
+              )
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
