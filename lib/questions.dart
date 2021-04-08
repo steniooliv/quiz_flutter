05bed7d98166068d0b39969919696d2d6bb447f5
Stenio Oliveira <steniooliv@gmail.com>
2021-04-07 22:08:28 -0300

    create questions component

---
+++ lib/questions.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Questao extends StatelessWidget {
+  final String texto;
+  Questao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(15),
+      child: Text(
+        texto,
+        style: TextStyle(
+          fontSize: 28,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
