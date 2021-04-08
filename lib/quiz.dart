1607c0b9c47c442021678be93362389a137a0dc2
Stenio Oliveira <steniooliv@gmail.com>
2021-04-07 23:45:04 -0300

    create quiz component

---
+++ lib/quiz.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_flutter/answer.dart';
+import 'package:quiz_flutter/questions.dart';
+
+class Questionario extends StatelessWidget {
+  final List<Map<String, Object>> perguntas;
+  final int perguntaSelecionada;
+  final void Function(int) responder;
+
+  Questionario({
+    @required this.perguntas,
+    @required this.perguntaSelecionada,
+    @required this.responder,
+  });
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, Object>> resposta = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada]["respostas"]
+        : null;
+
+    return Column(
+      children: <Widget>[
+        Questao(
+          perguntas[perguntaSelecionada]["texto"],
+        ),
+        ...resposta.map((resp) {
+          return Resposta(
+            resp["texto"],
+            () => responder(resp["pontuacao"]),
+          );
+        }).toList(),
+      ],
+    );
+  }
+}
